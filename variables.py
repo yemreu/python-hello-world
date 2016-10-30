7f73fe79b3316f33879d82c0f5da0e74276bc138
Y. Emre Uzun <yunusemreuzun@outlook.com>
2016-10-31 00:52:30 +0200

    Data Type: Tuple

--- variables.py
+++ variables.py
@@ -1,53 +1,64 @@
-#Assigning Values to Variables
-a=100; #integer
-b=10.2 #float
-c="py" #string
+# Assigning Values to Variables
+a = 100   # integer
+b = 10.2  # float
+c = "py"  # string
 
-print(a,b,c,end="\n\n")
+print(a, b, c, end="\n\n")
 
-#Multiple Assignment
-a=b=c="values changed"
-print(a,b,c,sep="\n",end="\n\n")
-a,b,c=0,1,"Py"
-print(a,b,c,sep="\n",end="\n\n")
+# Multiple Assignment
+a = b = c = "values changed"
+print(a, b, c, sep="\n", end="\n\n")
+a, b, c = 0, 1, "Py"
+print(a, b, c, sep="\n", end="\n\n")
 
-#Standard Data Types
-#-Numbers
-#-String
-#-List
-#-Tuple
-#-Dictionary
+# Standard Data Types
+# -Numbers
+# -String
+# -List
+# -Tuple
+# -Dictionary
 
-#--Numbers--
-a=10
-b=5
-print(a,b,end="\n\n")
+# --Numbers--
+a = 10
+b = 5
+print(a, b, end="\n\n")
 
-#Numerical Types: int, float, complex
+# Numerical Types: int, float, complex
 
-a=0xa1
-b=18.7e5
-c=12+3j
-print(a,b,c,end="\n\n")
+a = 0xa1
+b = 18.7e5
+c = 12+3j
+print(a, b, c, end="\n\n")
 
-#--Strings--
+# --Strings--
 
-str = "Hello World"
+pyStr = "Hello World"
 
-print(str)                      #string
-print(str[0])                   #first character
-print(str[2:5])                 #3. character to 5. character
-print(str[2:])                  #from 3. character to last
-print(str * 2)                  #print 2 times
-print(str + "Py",end="\n\n")    #concated string
+print(pyStr)                        # string
+print(pyStr[0])                     # first character
+print(pyStr[2:5])                   # 3. character to 5. character
+print(pyStr[2:])                    # from 3. character to last
+print(pyStr * 2)                    # print 2 times
+print(pyStr + "Py", end="\n\n")     # concated string
 
-#--Lists--
-listChr = ['a','b','c','d','e']
-listNum = [1,2]
+# --Lists--
+listChr = ['a', 'b', 'c', 'd', 'e']
+listNumeric = [1, 2]
 
-print(listChr)                  #list
-print(listChr[0])               #first element
-print(listChr[1:3])             #from 2. element to 3. element
-print(listChr[2:])              #from 2. element to last
-print(listNum * 2)              #print 2 times
-print(listChr + listNum)        #concated list
+print(listChr)                          # list
+print(listChr[0])                       # first element
+print(listChr[1:3])                     # from 2. element to 3. element
+print(listChr[2:])                      # from 2. element to last
+print(listNumeric * 2)                  # print 2 times
+print(listChr + listNumeric, end="\n\n")            # concated list
+
+# --Tuples--
+tupleChr = ('a', 'b', 'c', 'd', 'e')
+tupleNumeric = (1,2)
+
+print(tupleChr)                         # list
+print(tupleChr[0])                      # first element
+print(tupleChr[1:3])                    # from 2. element to 3. element
+print(tupleChr[2:])                     # from 2. element to last
+print(tupleNumeric * 2)                 # print 2 times
+print(tupleChr + tupleNumeric, end="\n\n")          # concated tuple
