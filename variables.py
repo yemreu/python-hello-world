cf8792fdb5785c9d760fb495829f61a0a940fe92
Y. Emre Uzun <yunusemreuzun@outlook.com>
2016-10-30 23:59:49 +0200

    Data Type: Numbers added

--- variables.py
+++ variables.py
@@ -7,4 +7,25 @@ print(a,b,c,end="\n\n")
 
 #Multiple Assignment
 a=b=c="values changed"
-print(a,b,c,sep="\n")
+print(a,b,c,sep="\n",end="\n\n")
+a,b,c=0,1,"Py"
+print(a,b,c,sep="\n",end="\n\n")
+
+#Standard Data Types
+#-Numbers
+#-String
+#-List
+#-Tuple
+#-Dictionary
+
+#--Numbers--
+a=10
+b=5
+print(a,b,end="\n\n")
+
+#Numerical Types: int, float, complex
+
+a=0xa1
+b=18.7e5
+c=12+3j
+print(a,b,c,end="\n\n")
