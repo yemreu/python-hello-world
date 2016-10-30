66a7c186e683cb06a4475a3901ed0be6e9cfe253
Y. Emre Uzun <yunusemreuzun@outlook.com>
2016-10-31 00:24:36 +0200

    Data Type: List

--- variables.py
+++ variables.py
@@ -34,9 +34,20 @@ print(a,b,c,end="\n\n")
 
 str = "Hello World"
 
-print(str)
-print(str[0])       #first character
-print(str[2:5])     #3. character to 5. character
-print(str[2:])      #from 3. character to last
-print(str * 2)      #print 2 times
-print(str + "Py")   #concat
+print(str)                      #string
+print(str[0])                   #first character
+print(str[2:5])                 #3. character to 5. character
+print(str[2:])                  #from 3. character to last
+print(str * 2)                  #print 2 times
+print(str + "Py",end="\n\n")    #concated string
+
+#--Lists--
+listChr = ['a','b','c','d','e']
+listNum = [1,2]
+
+print(listChr)                  #list
+print(listChr[0])               #first element
+print(listChr[1:3])             #from 2. element to 3. element
+print(listChr[2:])              #from 2. element to last
+print(listNum * 2)              #print 2 times
+print(listChr + listNum)        #concated list
