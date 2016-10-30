5a90f71fadb11b4cb6918f75e7dd3817a36f1201
Y. Emre Uzun <yunusemreuzun@outlook.com>
2016-10-30 21:51:46 +0200

    about code block

---
+++ code-block.py
@@ -0,0 +1,21 @@
+#Lines and Indentation
+if True:
+    print("ANSWER")
+    print("TRUE")
+else:
+    print("ANSWER")
+    print("FALSE")
+
+print()
+
+#Multi - Line Statements
+a=5+\
+  4+\
+  3
+print(a)
+
+print()
+
+days = ['monday','tuesday','wednesday',
+        'thursday','friday','saturday','sunday']
+print(days)
