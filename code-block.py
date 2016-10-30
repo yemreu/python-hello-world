9f90eaed926e6b967d44351b9a33443a9902cb1c
Y. Emre Uzun <yunusemreuzun@outlook.com>
2016-10-30 22:03:20 +0200

    about code block

--- code-block.py
+++ code-block.py
@@ -1,21 +1,24 @@
 #Lines and Indentation
 if True:
     print("ANSWER")
-    print("TRUE")
+    print("TRUE",end='\n\n')
 else:
     print("ANSWER")
-    print("FALSE")
+    print("FALSE",end='\n\n')
 
-print()
 
 #Multi - Line Statements
 a=5+\
   4+\
   3
-print(a)
-
-print()
+print(a,end='\n\n')
 
 days = ['monday','tuesday','wednesday',
         'thursday','friday','saturday','sunday']
 print(days)
+
+#Waiting for the User
+input("\n\nEnter to continue->|")
+
+#Multiple statements on a single line
+a=10;b=5;print("\n10+5=",a+b,sep="")
